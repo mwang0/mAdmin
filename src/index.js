bb93faf9d04b4278a64d4d02f7c859f5855354e8
NULL <xttnyw@gmail.com>
2019-05-20 11:14:25 +0800

    feat: 打包js, 检验webpack对 es6\CommonJS\AMD的支持

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world!')
+import sum from './vendor/sum'
+console.log('sum(1,2)' + sum(1, 2))
+
+const minus = require('./vendor/minus')
+console.log('minus(1,2)' + minus(1, 2))
+
+require(['./vendor/multi.js'], function(multi) {
+  console.log('multi(1,2)=' + multi(1, 2))
+})
---
+++ src/vendor/sum.js
@@ -0,0 +1,4 @@
+// es module
+export default function sum(a, b) {
+  return a + b
+}
