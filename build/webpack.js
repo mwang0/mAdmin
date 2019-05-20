16ce6a74efb79dd25d1fe7d85e56b5dda86dbbd4
NULL <xttnyw@gmail.com>
2019-05-20 16:29:14 +0800

    feat<config>
    es6/7/8 转 es5
    
    #1

---
+++ build/webpack.js
@@ -0,0 +1,19 @@
+/* 
+  webpack.js
+  webpack 公共配置
+  Created by M.Wang [cn_wang@139.com]
+  2019-05-20 15:59 Monday
+*/
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /src/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
